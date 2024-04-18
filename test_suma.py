9727c2d439ccff8b9ac8390316b1f060dd2ae9d3
IbrahimRiani <abrem1235@gmail.com>
2024-04-19 01:55:27 +0200

    editado test_sumar 1:55

--- test_suma.py
+++ test_suma.py
@@ -1,29 +1,33 @@
+# test_suma.py para el nuevo commit
+
 import unittest
 from suma import sumar, restar, multiplicar, dividir
 
-class TestOperaciones(unittest.TestCase):
+class TestSumar(unittest.TestCase):
+
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
 
     def test_restar(self):
-        self.assertEqual(restar(5, 2), 1)
         self.assertEqual(restar(5, 3), 2)
-        self.assertEqual(restar(-1, -1), 0)
+        self.assertEqual(restar(3, 5), -2)
+        self.assertEqual(restar(0, 0), 0)
 
     def test_multiplicar(self):
-        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(5, 3), 15)
         self.assertEqual(multiplicar(-1, 1), -1)
-        self.assertEqual(multiplicar(-1, -1), 1)
+        self.assertEqual(multiplicar(0, 5), 0)
 
     def test_dividir(self):
-        self.assertEqual(dividir(6, 2), 3)
-        self.assertEqual(dividir(9, 3), 3)
+        self.assertEqual(dividir(10, 2), 5)
+        self.assertEqual(dividir(10, 5), 2)
         self.assertEqual(dividir(-10, 2), -5)
         self.assertEqual(dividir(0, 5), 0)
         with self.assertRaises(ValueError):
-            dividir(5, 0)
+            dividir(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
+
